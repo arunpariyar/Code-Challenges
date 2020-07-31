d277526b4b2af71adf1c7bd4140929e20f2f93a8
Arun Pariyar <arun.pariyar@gmail.com>
2020-07-31 10:44:05 +0200

    Add Update

--- AdvancedJS/script.js
+++ AdvancedJS/script.js
@@ -1,7 +1,28 @@
+var random, userInput ;
+
+gamePlaying = true;
+
+
+
 function Question(question, answerOptions, answer){
     this.question = question;
     this.answerOptions = answerOptions;
     this.answer = answer;
+}
+
+Question.prototype.display = function() {
+    console.log(this.question);
+    for (var i = 0; i < this.answerOptions.length; i++){
+        console.log(i + ': '+ this.answerOptions[i] );
+    };
+}
+
+Question.prototype.checkAnswer = function(val) {
+    if(val === this.answer){
+        return true;
+    }else{ 
+        return false;
+    }
 }
 
 var q1 = new Question(
@@ -21,16 +42,36 @@ var q3 = new Question(
 
 var questions = [q1, q2, q3];
 
-var random =  Math.floor(Math.random() * questions.length);
+init();
 
-Question.prototype.display = function() {
-    console.log(this.question);
-    for (var i = 0; i < this.answerOptions.length; i++){
-        console.log(i + ': '+ this.answerOptions[i] );
-    };
+function init(){
+    r = randomNumber();
+    askQuestion(r);
+    userInput = Number(prompt("Please enter the right answer ?"));
+    result = questions[r].checkAnswer(userInput);
+    if(result === true){
+        console.log("Correct");
+        init();
+    }else{
+        console.log("Incorrect");
+        init();
+    }
 }
 
-questions[random].display();
+function askQuestion(r){
+    questions[r].display();
+}
+
+function randomNumber(){
+    return random =  Math.floor(Math.random() * questions.length);
+}
+ 
+
+
+
+
+
+
 
 
 
