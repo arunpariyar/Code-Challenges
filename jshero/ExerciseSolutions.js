72c5ca7be3a1da65e8d8ae78c0d41b93418c5445
Arun Pariyar <arun.pariyar@gmail.com>
2020-08-23 20:59:47 +0200

    JShero.net Contd

--- jshero/ExerciseSolutions.js
+++ jshero/ExerciseSolutions.js
@@ -556,3 +556,20 @@ function parseFirstInt(string){
 return numString;
     }  
 } */
+/* 
+Exercise
+Write a function add that takes a string with a summation task and returns its result as a number. A finite number of natural numbers should be added. The summation task is a string of the form '1+19+...+281'.
+
+Example: add('7+12+100') should return 119.
+
+My Solution
+
+function add(string){
+    let sum = 0;
+    let parts=string.split('+');
+    for(let i = 0; i<parts.length; i++){
+    
+    sum += parseInt(parts[i]);
+    }
+    return sum;
+} */
