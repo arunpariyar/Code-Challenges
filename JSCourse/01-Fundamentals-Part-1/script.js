d22a288d71b217a86b6b4c2cf03507997739c188
Arun Pariyar <arun.pariyar@gmail.com>
2020-11-26 17:09:34 +0100

    Lesson 10 Complete

---
+++ JSCourse/01-Fundamentals-Part-1/script.js
@@ -0,0 +1,11 @@
+/* LECTURE: Values and Variables */
+
+let country = "Nepal";
+let continent = "Asia";
+let population = 29136808;
+let isIsland = 0;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
